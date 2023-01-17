2201d127c2e6914753f4504dd97965ae0970d477
MithicSpirit <rpc01234@gmail.com>
2023-01-17 16:39:01 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+use serenity::client::ClientBuilder;
+use serenity::prelude::*;
+
+// Registers handlers and starts the bot
+#[tokio::main]
+async fn main()
+{
+	let token = env::var("DISCORD_TOKEN").unwrap_or_default();
+	// validate(&token).expect(format!("Invalid token `{token}'").as_str());
+	// ^ doesn't work for some reason
+
+	let intents = GatewayIntents::non_privileged()
+		| GatewayIntents::GUILD_MEMBERS;
+
+	let mut client = ClientBuilder::new(token, intents)
+		.await
+		.expect("Failed to create client");
+
+	if let Err(why) = client.start().await {
+		println!(
+			"An error occurred while running the client: {:?}",
+			why
+		);
+	}
+}
