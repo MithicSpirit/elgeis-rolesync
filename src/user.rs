9b6b875bf2fa7642731d5c44b5bcf629b7bf4ce8
MithicSpirit <rpc01234@gmail.com>
2023-01-19 21:00:04 -0500

    Most things for user.rs
    Left the hardest one for last (because it depends on role.rs)

--- src/user.rs
+++ src/user.rs
@@ -1,14 +1,34 @@
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 
+use crate::context::keys::*;
+
 pub async fn ban(ctx: &Context, user: User)
 {
-	todo!();
+	let data = ctx.data.read().await;
+	if let Err(why) = data
+		.get::<Config>()
+		.unwrap()
+		.target
+		.ban_with_reason(&ctx.http, user, 0, "Synchronized from Elgeis")
+		.await
+	{
+		println!("An error ocurred while banning a user: {:?}", why);
+	};
 }
 
 pub async fn unban(ctx: &Context, user: User)
 {
-	todo!();
+	let data = ctx.data.read().await;
+	if let Err(why) = data
+		.get::<Config>()
+		.unwrap()
+		.target
+		.unban(&ctx.http, user)
+		.await
+	{
+		println!("An error ocurred while unbanning a user: {:?}", why);
+	};
 }
 
 pub async fn sync(ctx: &Context, source: Member, target: Member)
@@ -18,5 +38,20 @@ pub async fn sync(ctx: &Context, source: Member, target: Member)
 
 pub async fn clean(ctx: &Context, mut member: Member)
 {
-	todo!();
+	let data = ctx.data.read().await;
+	let synced_roles = &data.get::<RevRoleMap>().unwrap();
+	let to_remove: Vec<RoleId> = member
+		.roles
+		.iter()
+		.filter(|role| synced_roles.contains_key(role))
+		.map(|r| *r)
+		.collect();
+	if let Err(why) =
+		member.remove_roles(&ctx.http, to_remove.as_slice()).await
+	{
+		println!(
+			"An error ocurred while removing a user's roles: {:?}",
+			why
+		)
+	};
 }
